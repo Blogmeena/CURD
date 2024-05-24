6d55646d60711336f4263ad689992641975d7396
Meena gupta <meenagarg1988@gmail.com>
2024-05-24 14:02:50 +0530

    model added

--- CRUD/Home/urls.py
+++ CRUD/Home/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.home),
+   path('show',views.show),
+   path('send',views.send),
 ]
--- CRUD/Home/views.py
+++ CRUD/Home/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from Home.models import Entry
 
 def home(request):
     return render(request,"home.html")
+
+def show(request):
+    return render(request,"show.html")
+
+def send(request):
+    if request.method == "POST":
+        ID=request.POST['id']
+        data1=request.POST['data1']
+        data2=request.POST['data2']
+        Entry(ID=ID, data1=data1, data2=data2).save()
+        msg="Data stored successfully"
+        return render(request,"home.html",{'msg':msg})
+    else:
+        return HttpResponse("<h1>404 - Not found</h1>")
