a89710571a8af65af0d7c110ffa2e483b2ee6800
Meena gupta <meenagarg1988@gmail.com>
2024-05-24 18:41:04 +0530

    CRUD commited

--- CRUD/Home/urls.py
+++ CRUD/Home/urls.py
@@ -4,4 +4,7 @@ urlpatterns = [
     path('',views.home),
    path('show',views.show),
    path('send',views.send),
+   path('delete',views.delete),
+   path('edit',views.edit),
+   path('RecordEdited',views.RecordEdited)
 ]
